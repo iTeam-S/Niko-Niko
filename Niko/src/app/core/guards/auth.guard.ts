0d93b34cfe7beda0eae4cec5d61e8b6087e8b33a
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-08-02 10:52:58 +0300

    use routes'guards...

---
+++ Niko/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, 
+    Router, RouterStateSnapshot } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        if(!this.authService.isLoggedIn()) {
+            this.router.navigateByUrl('/auth');
+            return false;
+        }
+        return this.authService.isLoggedIn();
+    }
+}
