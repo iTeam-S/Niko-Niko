5809c3175a7582cb9dbe20801429e3d0b2311a1b
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-09-07 11:16:47 +0300

    Pour voir le groupe qu'indique l'URL...

--- Niko/src/app/niko/components/groupe-created/groupe-created.component.ts
+++ Niko/src/app/niko/components/groupe-created/groupe-created.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ModelGroupCreated, ModelNomGroupe } from 'src/app/core/models/niko.model';
 import { NikoService } from 'src/app/core/services/niko.service';
 
 @Component({
@@ -8,13 +9,30 @@ import { NikoService } from 'src/app/core/services/niko.service';
   styleUrls: ['./groupe-created.component.scss']
 })
 export class GroupeCreatedComponent implements OnInit {
-
+  groupe_id!: number;
+  nom_groupe!: ModelNomGroupe;
+  groupeCreated!: ModelGroupCreated[];
+  iconeToast!: any;
+  titreToast!: string | null;
+  messageToast!: string | null;
   constructor(
     private nikoService: NikoService,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.groupe_id = +this.route.snapshot.params['id'];
+    this.nikoService.getNomGroupe(this.groupe_id).subscribe({
+      next: response => this.nom_groupe = response,
+      error: e => console.log(`Il y a une erreur...${ e }`)
+    });
+    this.nikoService.getMoodGroupes(this.groupe_id).subscribe({
+      next: response => this.groupeCreated = response,
+      error: (response) => {
+        this.iconeToast = "warning";
+        this.titreToast = "Erreur";
+        this.messageToast = response.error.message;
+      }
+    });
   }
-
 }
