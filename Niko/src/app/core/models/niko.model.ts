01f47b625e05630b77b5f01d0f90cabc01d31a91
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-09-07 11:10:04 +0300

    J'ai ajouté d'autres modèles de données pour les humeurs des personnes...

--- Niko/src/app/core/models/niko.model.ts
+++ Niko/src/app/core/models/niko.model.ts
@@ -26,4 +26,29 @@ export class ModelMyGroupe {
     id!: number;
     nom_groupe!: string;
     created_at!: Date;
+    groupe_id!: number;
+}
+
+export class ModelSingleGroupe {
+    lundi!: string;
+    mardi!: string;
+    mercredi!: string;
+    jeudi!: string;
+    vendredi!: string;
+    semaine!: Date;
+    prenom_usuel!: string;
+}
+
+export class ModelCreateHumeur {
+    humeur_type!: string;
+    groupe_id!: number;
+}
+
+export class ModelGroupCreated {
+    semaine_id!: Date;
+    humeurs!: ModelSingleGroupe[];
+}
+
+export class ModelNomGroupe {
+    nom_groupe!: string;
 }
